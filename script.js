553f9ed94347e5374902b7a755d17dee506dc221
AmaraLeandro <77883879+AmaraLeandro@users.noreply.github.com>
2021-02-28 14:37:18 -0400

    primeiro commit

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ function tabuada()  {
     if (num.value.length == 0 ) {
         window.alert('por favor, digite um numero!')
     }else{
+      
         let n =Number(num.value)
         let c = 1
         tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
-            item.txt = `${n} x ${c} =${n*c}`
+            item.text = `${n} x ${c} =${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
+            console.log(item)
             c++
         }
     }
